409b1e07a2fb211c7c06a7aa1e998ef3ba422b8e
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2025-03-12 17:12:37 +0100

    fix: auto-corr by rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:weight])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :weight ])
   end
 end
