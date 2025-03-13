40d75fffb014c42c0effb35c829902312ac44203
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2025-03-13 09:25:34 +0100

    chore: trying to find a soluce agains it

--- test.rb
+++ test.rb
@@ -1,11 +1,13 @@
-require 'mysql2'
+require "mysql2"
 
 # Connexion à la base de données en utilisant les variables d'environnement
 client = Mysql2::Client.new(
-  host: ENV['DATABASE_HOST'], 
-  username: ENV['DATABASE_USER'], 
-  password: ENV['DATABASE_PASSWORD'], 
-  database: ENV['DATABASE_NAME']
+  host: ENV["DATABASE_HOST"],
+  username: ENV["DATABASE_USERNAME"],
+  password: ENV["DATABASE_PASSWORD"],
+  database: ENV["DATABASE_NAME"],
+  port: ENV["DATABASE_PORT"],
+  url: ENV["DATABASE_URL"], # Utilise l'URL définie dans les variables d'environnement
 )
 
 puts "Connexion réussie à la base de données MySQL!"
