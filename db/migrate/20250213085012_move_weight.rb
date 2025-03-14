2289b7cd42d84f7c548dd3107975c7497d34c6f1
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2025-03-14 09:15:59 +0100

    fix: bad order in a migration

--- db/migrate/20250213085012_move_weight.rb
+++ db/migrate/20250213085012_move_weight.rb
@@ -1,7 +1,6 @@
 class MoveWeight < ActiveRecord::Migration[7.1]
   def change
-    add_column :product_records, :weight, :decimal, precision: 5, scale: 2
-
     remove_column :users, :weight
+    add_column :product_records, :weight, :decimal, precision: 5, scale: 2
   end
 end
